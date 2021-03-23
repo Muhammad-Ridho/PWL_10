d9544805a79d4c67e2cfa4772e5576f04319a860
= <ramadhanjenius1@gmail.com>
2021-03-24 02:24:04 +0700

    edit file migration table mahasiswa

--- database/migrations/2021_03_23_185238_create_mahasiswas_table.php
+++ database/migrations/2021_03_23_185238_create_mahasiswas_table.php
@@ -14,8 +14,13 @@ class CreateMahasiswasTable extends Migration
     public function up()
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('Nim', 10)->index();
+            $table->string('Nama', 100)->nullable();
+            $table->string('Kelas', 20)->nullable();
+            $table->string('Jurusan', 100)->nullable();
+            $table->string('No_Handphone', 12)->nullable();
+
+
         });
     }
 
