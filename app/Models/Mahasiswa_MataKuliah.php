ad67dc5f7252c7e099e8dd47dee632e2f75aa024
= <ramadhanjenius1@gmail.com>
2021-04-13 09:58:24 +0700

    edit model Mahasiswa_MataKuliah

--- app/Models/Mahasiswa_MataKuliah.php
+++ app/Models/Mahasiswa_MataKuliah.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa_MataKuliah extends Model
 {
     use HasFactory;
+    protected $table = 'mahasiswa_matakuliah';
+
+    public function mahasiswa(){
+        return $this->belongsTo(Mahasiswa::class, 'mahasiswa_id', 'Nim');
+    }
+    
+    public function matakuliah(){
+        return $this->belongsTo(MataKuliah::class);
+    }
 }
